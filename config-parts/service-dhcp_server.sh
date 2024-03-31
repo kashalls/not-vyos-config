3279d30af9034a9270ab0d37811f94e8b6fe76df
Jordan Jones <jordpjones@gmail.com>
2024-03-31 14:32:10 -0700

    Fucking templates.

--- config-parts/service-dhcp_server.sh
+++ config-parts/service-dhcp_server.sh
@@ -84,7 +84,7 @@ set service dhcp-server shared-network-name SERVERS subnet 10.69.0.0/24 subnet-p
 set service dhcp-server shared-network-name SERVERS subnet 10.69.0.0/24 tftp-server-name 10.1.0.1
 # "PXEClient:Arch:00000:UNDI:002001" is the vendor-class-identifier that is unique to all Raspberry Pi Clients (tested 3B+ -> 4B 8GB)
 set service dhcp-server shared-network-name SERVERS subnet 10.69.0.0/24 subnet-parameters 'if exists user-class and option user-class = &quot;iPXE&quot; { filename &quot;http://10.5.0.8/boot.ipxe&quot;; }'
-set service dhcp-server shared-network-name SERVERS subnet 10.69.0.0/24 subnet-parameters 'else if option system-arch = 00:19 { filename &quot;ipxe-arm64.efi&quot;; }'
+set service dhcp-server shared-network-name SERVERS subnet 10.69.0.0/24 subnet-parameters 'else if option system-arch = 00:11 { filename &quot;http://10.5.0.8/boot.ipxe&quot;; }'
 # https://xunnanxu.github.io/2020/11/28/PXE-Boot-Diskless-Raspberry-Pi-4-With-Ubuntu-Ubiquiti-and-Synology-1-DHCP-Setup/
 set service dhcp-server shared-network-name SERVERS subnet 10.69.0.0/24 subnet-parameters 'else if option system-arch = 00:00 { bootfile-name &quot;ipxe-arm64.efi&quot;; option vendor-class-identifier &quot;PXEClient&quot;; option vendor-encapsulated-options &quot;Raspberry Pi Boot&quot;; }'
 set service dhcp-server shared-network-name SERVERS subnet 10.69.0.0/24 subnet-parameters 'else { filename &quot;ipxe.efi&quot;; }'
