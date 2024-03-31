bccc1c2a58d8a4f7e4e38f65af8f1d921cdaff15
Jordan Jones <jordpjones@gmail.com>
2024-03-31 00:16:17 -0700

    trying this

--- bootstrap/tftp.sh
+++ bootstrap/tftp.sh
@@ -7,11 +7,11 @@ curl -L -o /config/tftpboot/ipxe.efi http://boot.ipxe.org/ipxe.efi
 curl -L -o /config/tftpboot/undionly.kpxe http://boot.ipxe.org/undionly.kpxe
 
 # Raspberry Pi TFTP
-mkdir -p /tmp/raspi-tftp
-curl -o /tmp/raspi-tftp/rpi4-uefi-ipxe.zip https://github.com/rgl/rpi4-uefi-ipxe/releases/latest/download/rpi4-uefi-ipxe.zip
-unzip -d /tmp/raspi-tftp/rpi4-uefi-ipxe tmp/raspi-tftp/rpi4-uefi-ipxe.zip
-cp /tmp/raspi-tftp/rpi4-uefi-ipxe/efi/boot/bootaa64.efi /config/tftpboot/ipxe-arm64.efi
+mkdir -p /config/raspi-tftp
+curl -L -o /config/raspi-tftp/rpi4-uefi-ipxe.zip https://github.com/rgl/rpi4-uefi-ipxe/releases/latest/download/rpi4-uefi-ipxe.zip
+unzip -d /config/raspi-tftp/rpi4-uefi-ipxe /config/raspi-tftp/rpi4-uefi-ipxe.zip
+cp /config/raspi-tftp/rpi4-uefi-ipxe/efi/boot/bootaa64.efi /config/tftpboot/ipxe-arm64.efi
 
-rm -rf /tmp/raspi-tftp
+rm -rf /config/raspi-tftp
 
 sudo chown -R tftp:tftp /config/tftpboot/
